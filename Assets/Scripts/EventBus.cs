6d4f44e8aeb201854976f06fda82925b6a2a5000
Tu Nombre <tu@email.com>
2025-08-12 07:56:52 -0500

    EventBus

--- Assets/Scripts/EventBus.cs
+++ Assets/Scripts/EventBus.cs
@@ -8,23 +8,57 @@ public static class EventBus
 
     public static void Subscribe<T>(string eventName, Action<T> listener)
     {
-        if (_eventListener.ContainsKey(eventName))
+        if (!_eventListener.ContainsKey(eventName))
         {
-            _eventListener.Add(eventName, new());
+            _eventListener.Add(eventName, new List<Delegate>());
         }
         _eventListener[eventName].Add(listener);
-    }   
+    }
+
+    public static void Subscribe(string eventName, Action listener)
+    {
+        if (!_eventListener.ContainsKey(eventName))
+        {
+            _eventListener.Add(eventName, new List<Delegate>());
+        }
+        _eventListener[eventName].Add(listener);
+    }
 
     public static void Unsubscribe<T>(string eventName, Action<T> listener)
     {
-        if( _eventListener.ContainsKey(eventName))
+        if(_eventListener.ContainsKey(eventName))
         {
             _eventListener[eventName].Remove(listener);
         }
     }
 
-    public static void Raise(string eventName)
+    public static void Unsubscribe(string eventName, Action listener)
     {
+        if (_eventListener.ContainsKey(eventName))
+        {
+            _eventListener[eventName].Remove(listener);
+        }
+    }
 
+    public static void InvokeEvent<T>(string eventName, T EventData)
+    {
+        if (_eventListener.ContainsKey(eventName))
+        {
+            foreach (var listener in _eventListener[eventName]) 
+            {
+                ((Action<T>)listener).Invoke(EventData);
+            }
+        }
+    }
+
+    public static void InvokeEvent(string eventName)
+    {
+        if (_eventListener.ContainsKey(eventName))
+        {
+            foreach (var listener in _eventListener[eventName])
+            {
+                ((Action)listener).Invoke();
+            }
+        }
     }
 }
